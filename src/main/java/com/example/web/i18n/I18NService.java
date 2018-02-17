c4d27cfa58b836f3640dace81f6a6a402ee52306
Przemek Osiecki <przemyslaw.osiecki@gmail.com>
2018-02-17 14:32:39 +0100

    Add i18nService

--- src/main/java/com/example/web/i18n/I18NService.java
+++ src/main/java/com/example/web/i18n/I18NService.java
@@ -1,4 +1,24 @@
 package com.example.web.i18n;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
 public class I18NService {
+
+    @Autowired
+    MessageSource messageSource;
+
+    public String getMessage(String messageId) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return getMessage(messageId, locale);
+    }
+
+    private String getMessage(String messageId, Locale locale) {
+        return messageSource.getMessage(messageId, null, locale);
+    }
 }
--- src/test/java/com/example/web/i18n/I18NServiceIntegrationTest.java
+++ src/test/java/com/example/web/i18n/I18NServiceIntegrationTest.java
@@ -1,4 +1,28 @@
 package com.example.web.i18n;
 
+import com.example.WebappApplication;
+import com.example.config.I18nConfig;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = WebappApplication.class)
 public class I18NServiceIntegrationTest {
+    @Autowired
+    I18NService i18NService;
+
+    @Test
+    public void shouldReturnAppropriateMessageFromMessageSource() {
+        //given
+        final String expected = "Bootstrap starter template";
+        final String messageId = "index.main.callout";
+        //when
+        final String actual = i18NService.getMessage(messageId);
+        //then
+        Assert.assertEquals("Given message does not match", expected, actual);
+    }
 }
