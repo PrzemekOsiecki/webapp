0b62b28ffc9a6045e44c317b1c91ce3489eea067
Przemek Osiecki <przemyslaw.osiecki@gmail.com>
2018-02-24 21:40:40 +0100

    Added Lombok Project

--- src/main/java/com/example/web/domain/frontend/Feedback.java
+++ src/main/java/com/example/web/domain/frontend/Feedback.java
@@ -1,7 +1,11 @@
 package com.example.web.domain.frontend;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter @Setter
 public class Feedback implements Serializable {
 
     private static final long serialVersionUID = 1l;
@@ -22,35 +26,4 @@ public class Feedback implements Serializable {
         return sb.toString();
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFeedback() {
-        return feedback;
-    }
-
-    public void setFeedback(String feedback) {
-        this.feedback = feedback;
-    }
 }
